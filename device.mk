771cce97c86c82e0b39577a7b7f6616137d94acd
Arefin <ahmedarefinsajjad@gmail.com>
2023-05-25 01:25:14 +0600

    mojito: Add maintainer overlay

--- device.mk
+++ device.mk
@@ -312,6 +312,7 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     MojitoCarrierConfig \
     MojitoFrameworks \
+    MojitoMaintainer \
     MojitoSettingsProvider \
     MojitoSettings \
     MojitoSystemUI \
